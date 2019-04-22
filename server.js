cd222311bb2c84377c3c2d88db679dbfabc2d8fa
Johan Satgé <johansatge@users.noreply.github.com>
2019-04-22 22:13:10 +0200

    Enable gzip on dev server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const compression = require('compression')
 const express = require('express')
 const path = require('path')
 
 const app = express()
 const port = 4000
 
+app.use(compression())
 app.use((request, response, next) => {
   response.set('Cache-Control', 'no-cache, no-store, must-revalidate, max-age=0')
   next()
