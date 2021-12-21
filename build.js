c3a38b1550ecea870ae2ec8e5bc91953dad14eb4
Johan Satgé <johansatge@users.noreply.github.com>
2021-12-21 11:53:05 +0100

    Move to async/await

--- build.js
+++ build.js
@@ -10,22 +10,25 @@ const webpackConfig = require('./app/webpack.config.js')
 const distDir = path.join(__dirname, '.dist')
 const { FITBOOK_DROPBOX_APP_KEY } = require('./.env.js')
 
-const startTime = new Date().getTime()
-cleanDist()
-  .then(buildEjsTemplates)
-  .then((ejsTemplates) => {
-    return Promise.all([buildWebpack(ejsTemplates), buildIcons()])
-  })
-  .then(([assets, icons]) => {
-    return Promise.all([renderIndexHtml({ assets, icons }), renderLoginHtml({ assets })])
-  })
-  .then(() => {
+build()
+
+async function build() {
+  try {
+    const startTime = new Date().getTime()
+    await cleanDist()
+    const ejsTemplates = await buildEjsTemplates()
+    const assets = await buildWebpack(ejsTemplates)
+    const icons = await buildIcons()
+    await renderIndexHtml({ assets, icons })
+    await renderLoginHtml({ assets })
     const endTime = new Date().getTime()
     log(`Done in ${endTime - startTime}ms`)
-  })
-  .catch((error) => {
-    log(`An error occurred (${error})`)
-  })
+  } catch (error) {
+    log(error.message)
+    log(error.stack)
+    process.exit(1)
+  }
+}
 
 async function cleanDist() {
   log(`Cleaning ${distDir}`)
@@ -37,46 +40,42 @@ async function cleanDist() {
   await fsp.mkdir(distDir, { recursive: true })
 }
 
-function buildEjsTemplates() {
+async function buildEjsTemplates() {
   log('Building EJS templates')
-  return Promise.all([buildEjsTemplate('app/feed.ejs'), buildEjsTemplate('app/add.ejs')]).then(([feed, add]) => {
-    return { feed, add }
-  })
+  const feed = await buildEjsTemplate('app/feed.ejs')
+  const add = await buildEjsTemplate('app/add.ejs')
+  return { feed, add }
 }
 
-function buildEjsTemplate(templatePath) {
-  return fsp.readFile(templatePath, 'utf8').then((contents) => {
-    const compiled = ejs.compile(contents, {
-      client: true,
-      compileDebug: false,
-      strict: true,
-      localsName: 'data',
-    })
-    return compiled.toString()
+async function buildEjsTemplate(templatePath) {
+  const contents = await fsp.readFile(templatePath, 'utf8')
+  const compiled = ejs.compile(contents, {
+    client: true,
+    compileDebug: false,
+    strict: true,
+    localsName: 'data',
   })
+  return compiled.toString()
 }
 
-function buildWebpack(ejsTemplates) {
+async function buildWebpack(ejsTemplates) {
   log('Building webpack assets')
   const config = webpackConfig({ dropboxAppKey: FITBOOK_DROPBOX_APP_KEY, ejsTemplates })
-  return promisify(webpack)(config).then((stats) => {
-    stats = stats.toJson()
-    if (stats.errors.length > 0) {
-      throw new Error(stats.errors[0])
-    }
-    stats.warnings.forEach((warning) => {
-      log(`Webpack warning: ${warning}`)
-    })
-    return stats.assetsByChunkName
+  const stats = (await promisify(webpack)(config)).toJson()
+  if (stats.errors.length > 0) {
+    throw new Error(stats.errors[0])
+  }
+  stats.warnings.forEach((warning) => {
+    log(`Webpack warning: ${warning}`)
   })
+  return stats.assetsByChunkName
 }
 
-function buildIcons() {
+async function buildIcons() {
   log('Building SVG icons')
-  return promisify(glob)(path.join(__dirname, 'app/icons/*.svg')).then((files) => {
-    return Promise.all(files.map((file) => fsp.readFile(file, 'utf8'))).then((svgFiles) => {
-      return svgFiles.map((svg, index) => insertIconIdInSvg(files[index], svg)).join('\n')
-    })
+  const files = await promisify(glob)(path.join(__dirname, 'app/icons/*.svg'))
+  return Promise.all(files.map((file) => fsp.readFile(file, 'utf8'))).then((svgFiles) => {
+    return svgFiles.map((svg, index) => insertIconIdInSvg(files[index], svg)).join('\n')
   })
 }
 
@@ -85,30 +84,28 @@ function insertIconIdInSvg(filePath, svg) {
   return svg.replace('<svg ', `<svg id="svg-${fileName[1]}" `)
 }
 
-function renderIndexHtml({ assets, icons }) {
+async function renderIndexHtml({ assets, icons }) {
   log('Rendering HTML (index)')
-  return fsp.readFile(path.join(__dirname, 'app/index.ejs'), 'utf8').then((ejsTemplate) => {
-    const html = ejs.render(ejsTemplate, {
-      assets,
-      icons,
-      appTitle: pkg.name,
-      appTitleFull: `${pkg.name} ${pkg.version}`,
-    })
-    return fsp.writeFile(path.join(distDir, 'index.html'), html, 'utf8')
+  const ejsTemplate = await fsp.readFile(path.join(__dirname, 'app/index.ejs'), 'utf8')
+  const html = ejs.render(ejsTemplate, {
+    assets,
+    icons,
+    appTitle: pkg.name,
+    appTitleFull: `${pkg.name} ${pkg.version}`,
   })
+  await fsp.writeFile(path.join(distDir, 'index.html'), html, 'utf8')
 }
 
-function renderLoginHtml({ assets }) {
+async function renderLoginHtml({ assets }) {
   log('Rendering HTML (login)')
-  return fsp.readFile(path.join(__dirname, 'app/login.ejs'), 'utf8').then((ejsTemplate) => {
-    const html = ejs.render(ejsTemplate, {
-      assets,
-      appTitle: pkg.name,
-      appTitleFull: `${pkg.name} ${pkg.version}`,
-      dropboxAppKey: FITBOOK_DROPBOX_APP_KEY,
-    })
-    return fsp.writeFile(path.join(distDir, 'login.html'), html, 'utf8')
+  const ejsTemplate = await fsp.readFile(path.join(__dirname, 'app/login.ejs'), 'utf8')
+  const html = ejs.render(ejsTemplate, {
+    assets,
+    appTitle: pkg.name,
+    appTitleFull: `${pkg.name} ${pkg.version}`,
+    dropboxAppKey: FITBOOK_DROPBOX_APP_KEY,
   })
+  await fsp.writeFile(path.join(distDir, 'login.html'), html, 'utf8')
 }
 
 function log(message) {
